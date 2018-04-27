b2ea89967236a79784a941202b5ad2a00534e485
chunxin liu <will1990liu@163.com>
2018-04-27 20:26:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,15 +4,15 @@ function add(a,b){
   return a+b
 }
 
-function subtract(x,y){
-  return x-y
+function subtract(a,b){
+  return a-b
 }
 
-function multiply(x,y){
-  return x*y
+function multiply(a,b){
+  return a*b
 }
-function divide(x,y){
-  return x/y
+function divide(a,b){
+  return a/b
 }
 
 function inc(n){
